80624970b5ad2d1c1947bc4110bbe7f1301cd461
Anuradha Khambete <anukhambete@gmail.com>
2017-12-19 15:29:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 function addElementToBeginningOfArray(Array,foo){
   return ['foo',...Array]
 }
+
+function destructivelyaddElementToBeginningOfArray(Array,foo){
+  Array.unshift('foo')
+  return Array
+}
